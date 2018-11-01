a5bdf8d309ab45d86e54b0e2e12ac2b543e5e838
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-11-01 17:37:41 +0000

    Only tweet today's tweets

--- tweet.py
+++ tweet.py
@@ -1,14 +1,31 @@
+from __future__ import print_function
 from twitter import *
 from get_jams import get_jams
 from datetime import datetime
 import config
+import sys
+
+test = False
+if "test" in sys.argv:
+    test = True
 
 twitter = Twitter(auth=OAuth(config.access_key, config.access_secret,
                              config.consumer_key, config.consumer_secret))
 
 accounts = get_jams(datetime.now())
 
+def today(tweet):
+    now = datetime.now()
+    if now.strftime("%Y") not in tweet["created_at"]:
+        return False
+    if now.strftime("%b %d") not in tweet["created_at"]:
+        return False
+    return True
+
 for jam in accounts:
     for tweet in twitter.statuses.user_timeline(screen_name=jam, count=10):
-        if not tweet["retweeted"]:
-            twitter.statuses.retweet(id=tweet["id"])
+        if not tweet["retweeted"] and today(tweet):
+            if test:
+                print("If not testing, I would retweet this:",tweet["text"])
+            else:
+                twitter.statuses.retweet(id=tweet["id"])
